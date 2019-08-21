271a638cf59af5ba593b2b48c697844a81871700
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-21 20:40:47 +0200

    for loop

--- simple_for-loop.py
+++ simple_for-loop.py
@@ -15,5 +15,16 @@ print('\nAs you might have noticed by now, we love pizza')
 
 #####
 
+# Animals: Think of at least three different animals that have a common char-acteristic . Store the names of these
+# animals in a list, and then use a for loop to print out the name of each animal .• Modify your program to print a
+# statement about each animal, such as A dog would make a great pet.• Add a line at the end of your program stating
+# what these animals have in common . You could print a sentence such as Any of these animals would make a great pet!
 
+# >>
+animals = ['Kangaroo', 'Zebra', 'Giraffe', 'Elephant']
+
+for animal in animals:
+    print("Y'know What's absolutely CUTE? {} CUBS!!".format(animal))
+
+print("\nWhat do they have in common?: ummm, a whole CUTENESS DUH")
 
