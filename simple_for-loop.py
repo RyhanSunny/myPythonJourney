3dcf8a5acb4daa2ad376f8afc43c9cdaac612dd9
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-21 20:40:24 +0200

    for loop

---
+++ simple_for-loop.py
@@ -0,0 +1,19 @@
+# Pizzas: Think of at least three kinds of your favorite pizza . Store these pizza names in a list, and then use a
+# for loop to print the name of each pizza .• Modify your for loop to print a sentence using the name of the pizza
+# instead of printing just the name of the pizza . For each pizza you should have one line of output containing a
+# simple statement like I like pepperoni pizza .• Add a line at the end of your program, outside the for loop,
+# that states how much you like pizza . The output should consist of three or more lines about the kinds of pizza you
+# like and then an additional sentence, such as I really love pizza!
+
+# >>>
+pizzas = ['marinara', 'mozzarella', 'mushrooms', 'Pepperoni Pie']
+
+for pizza in pizzas:
+    print('Everyday is Pizza Day and Today We Want {}'.format(pizza))
+
+print('\nAs you might have noticed by now, we love pizza')
+
+#####
+
+
+
