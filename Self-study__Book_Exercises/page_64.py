3d4ac720f437695339afe755676fbf3f652871d3
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-24 16:38:04 +0200

    updated - till ex: 4.6

--- Self-study__Book_Exercises/page_64.py
+++ Self-study__Book_Exercises/page_64.py
@@ -9,17 +9,20 @@ print(list1)
 # If the output is taking too long, stop it by pressing ctrl-C or by closing the output window)
 #
 oneMill = [i for i in range(1, 1000001)]
-for n in oneMill:
-    print(n)
+# for n in oneMill:
+    # print(n)
 
 # 4-5. Summing a Million: Make a list of the numbers from one to one million, and then use min() and max() to make
 # sure your list actually starts at one and ends at one million . Also, use the sum() function to see how quickly
 # Python can add a million numbers
 
 oneMill = [i for i in range(1, 1000001)]
-print('min of 1 mill: '+str(min(oneMill)))
-print('max of 1 mill: '+str(max(oneMill)))
-print('sum: ' +str(sum(oneMill)))
+print('min of 1 mill: ' + str(min(oneMill)))
+print('max of 1 mill: ' + str(max(oneMill)))
+print('sum: ' + str(sum(oneMill)))
 
 # 4-6. Odd Numbers: Use the third argument of the range() function to make a list of the odd numbers from 1 to 20 .
 # Use a for loop to print each number.
+for i in range(1, 20, 2):
+    print(i)
+
