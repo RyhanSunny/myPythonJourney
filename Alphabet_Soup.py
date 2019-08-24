eb12fa54584e3a2637d6a13113c2e996c49cc955
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-24 17:55:58 +0200

    done

--- Alphabet_Soup.py
+++ Alphabet_Soup.py
@@ -1,3 +1,14 @@
 # This challenge requires you to alphabetically sort the characters in a string. We'll sort the characters using the
 # built-in array sort function.
 
+def AlphabetSoup(str):
+    # convert the string into a list of characters
+    chars = list(str)
+
+    # sort the list in alphabetical order
+    sortedChars = sorted(chars)
+
+    # return the newly joined string
+    return "".join(sortedChars)
+
+
