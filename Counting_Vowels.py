347ec128b03cc3076c70b3e6f694e02325adf1e2
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-24 18:10:43 +0200

    init

---
+++ Counting_Vowels.py
@@ -0,0 +1,14 @@
+# This challenge requires you to return the number of vowels (a, e, i, o, u) in a string. For example, if the string
+# is "All cows eat grass" then your program should return the integer 5. The first program is a basic loop that keeps
+# track of the vowels it encounters.
+
+string = str(input('Enter a sentence: ').strip())
+
+vowels = {'aeiouAEIOU'}
+for char in string:
+    if char in vowels:
+        vowels[char] += 1
+
+
+for i, j in vowels.:
+    print(k, v)
