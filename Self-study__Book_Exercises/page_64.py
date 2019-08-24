b328ccfca4e74b214d72b949a0a6de025e17ba51
Ryhan Sunny <SUNNYRAYHAN2@GMAIL.COM>
2019-08-24 16:49:38 +0200

    updated - till ex: 4.8

--- Self-study__Book_Exercises/page_64.py
+++ Self-study__Book_Exercises/page_64.py
@@ -29,3 +29,10 @@ for i in range(1, 20, 2):
 # 4 -7. Threes: Make a list of the multiples of 3 from 3 to 30. Use a for loop to print the numbers in your list
 for i in range(3, 30, 3):
     print(i)
+
+# 4-8. Cubes: Make a list of the first 10 cubes (that is, the cube of each integer from 1 through 10), and use a for
+# loop to print out the value of each cube
+cubes = [value**3 for value in range(1,10)]
+print('\n')
+for cube in cubes:
+    print(cube)
